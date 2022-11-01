01aa7d10daddec4d53a745d88f70a1a4c07d4ad1
Jose Maria Madroñal <jmmadronal@gmail.com>
2022-11-01 19:16:14 +0100

    first commit

---
+++ CON-UBS-22-DHCP/dhcpd_curl.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Purpose: Config ubuntu DHCP server
+# https://ubuntu.com/server/docs/network-dhcp
+# Author: Jose Maria Madronal GPL v2.0+ (Proxmox script)
+# ------------------------------------------
+#
+#
+# declare STRING variable
+STRING="Scripts examples install DHCP SERVICE"
+#print variable on a screen
+echo $STRING
+
+# declare variables to download files from private repository
+TOKEN=""
+headerToken="--header 'Authorization: token $TOKEN'"
+OWNER="josemol"
+REPO="S2I"
+PATH="main/CON-UBS-22-DHCP/etc/dhcp/dhcpd.conf"
+f_dchpd_conf="https://api.github.com/repos/$OWNER/$REPO/contents/$PATH"
+
+#Install DHCP
+apt-get install isc-dhcp-server
+
+# download githup configuration file
+curl $headerToken \
+     --header 'Accept: application/vnd.github.v3.raw' \
+     --remote-name \
+     --location $f_dchpd_conf
+
+# copy configuration file to etc directory
+cp dhcpd.conf /etc/dhcp
+
+systemctl restart isc-dhcp-server
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 # S2I
+## Introducció
+Aquest és un exemple de com fer un scriprt de instal·lació utilitzant un repositori de codi com el github.
+
